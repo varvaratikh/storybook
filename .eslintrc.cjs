3a5ffbdd64d58f784a801976a3b3f412a18840b4
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-06-09 18:38:02 +0300

    added

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: './tsconfig.eslint.json',
+        tsconfigRootDir: __dirname,
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    rules: {
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'no-unused-vars': 'warn',
+        'react/react-in-jsx-scope': 'off',
+    },
+};
